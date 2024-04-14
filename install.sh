917c362f51ae56f92512095ce0ec5f8991d4fdb6
liu-b <liu-b@klab.com>
2024-04-14 19:26:26 +0900

    chore(install.sh): add debug logging to display BASENAME, checksums, and content of checksums file for troubleshooting hash_sha256_verify function

--- install.sh
+++ install.sh
@@ -328,6 +328,9 @@ hash_sha256_verify() {
     return 1
   fi
   BASENAME=${TARGET##*/}
+  echo "${BASENAME}"
+  echo "${checksums}"
+  cat "${checksums}"
   want=$(grep "${BASENAME}" "${checksums}" 2>/dev/null | tr '\t' ' ' | cut -d ' ' -f 1)
   if [ -z "$want" ]; then
     log_err "hash_sha256_verify unable to find checksum for '${TARGET}' in '${checksums}'"
